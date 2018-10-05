a26de9f59e3bcebb562482db0f917c6cd1368859
wangpt <1315070220@qq.com>
2018-10-05 21:06:16 +0800

    Fix cache demo

--- TJCache/PersonInfoModel.h
+++ TJCache/PersonInfoModel.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface PersonInfoModel : NSObject<NSCoding>
+@interface PersonInfoModel : NSObject <NSCoding>
 @property (nonatomic, strong) NSString *name;
 @property (nonatomic, strong) NSString *sex;
 @property (nonatomic, assign) NSInteger age;
